ee96c08de5831a050c77b47d1d229db79a386214
hodgeslo <895933+hodgeslo@users.noreply.github.com>
2023-01-10 00:16:53 -0500

    Update app.py
    Keep pushing

--- app.py
+++ app.py
@@ -26,15 +26,6 @@ PLAYERS = [{
 """
 
 
-def display_menu():
-    print(f"\nBASKETBALL TEAM STATS TOOL\n")
-    print(f"------ MENU ------\n")
-    print(f"  Here are your choices:")
-    print(f"    A) Display Team Stats")
-    print(f"    B) Quit")
-    input("  Enter an option: ")
-
-
 # 4. Create a clean_data function
 def clean_data(players):
     cleaned_players_list = []
@@ -51,40 +42,51 @@ def clean_data(players):
 
 # 5. Create a balance_teams function
 def balance_teams(players, teams):
-    num_players_team = int(len(players) / len(teams))
-    cleaned_player_list = clean_data(players)
+    num_players_per_team = int(len(players) / len(teams))
+    cleaned_players_list = clean_data(players)
+    #print(cleaned_players_list)
+    #print(teams)
     initial_team_list = teams
     balanced_team_list = []
     players_only_set = set()
     players_only_list = []
     players_only_dict = {}
 
-    # print(f"This is a set: {players_only_set}")
-    # print(f"This is a list: {players_only_list}")
-    # print(f"This is a dict: {players_only_dict}")
-    # print(f"\n\n")
+    for index_players, number_players in enumerate(cleaned_players_list):
+        print(index_players, number_players)
+        #print(cleaned_players_list[:6][0].items())
 
-    for key in cleaned_player_list:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # START AGAIN ABOVE THIS LINE
+
+    for key in cleaned_players_list:
         # players_only_set.add(key['name'])
         players_only_list.append(key['name'])
 
-    # print(f"This is a set: {players_only_set}")
-    # print(f"This is a list: {players_only_list}")
-    # print(f"This is a dict: {players_only_dict}")
-    # print(f"\n\n")
+    # for i in range(num_players_team):
+    #     players_only_set.add(players_only_list[i])
+    #     # balanced_team_list.append(players_only_list[i])
 
-    for i in range(num_players_team):
-        players_only_set.add(players_only_list[i])
-        # balanced_team_list.append(players_only_list[i])
-
-    temp_player_set0 = players_only_set
-
-    print(f'temp player set 1: {temp_player_set0}')
-
-    print(f"This is a set: {players_only_set}")
-    print(f"This is a list: {players_only_list}")
-    print(f"This is a dict: {players_only_dict}")
-    print(f"\n\n")
+    for idx_players, num_players in enumerate(cleaned_players_list):
+        players_only_set.add(players_only_list[idx_players])
+        # cleaned_player_list.pop(idx_players)
+        print(idx_players, num_players)
 
     # for i in range(len(teams)):
     #     players_only_dict[initial_team_list[i]] = players_only_set
@@ -92,48 +94,90 @@ def balance_teams(players, teams):
     for idx, team_name in enumerate(teams):
         players_only_dict[team_name] = players_only_set
 
-    print(f"This is a set: {players_only_set}")
-    print(f"This is a list: {players_only_list}")
-    print(f"This is a dict: {players_only_dict}")
+    # print(f"This is a set: {players_only_set}")
+    # print(f"This is a list: {players_only_list}")
+    # print(f"This is a dict: {players_only_dict}")
     print(f"\n\n")
-    # print(players_only_dict['Panthers'])
-    # print(balanced_team_list)
 
-    # print(players_only_set)
-    # print(players_only_dict)
+    """
+    Here is where display the team stats
+    """
+    number_of_teams = len(teams)
+    for idx, team_name_menu in enumerate(teams):
+        print(f" {idx + 1})  {team_name_menu}")
 
-    # print(players_only_list)
+    team_menu_user_selected = False
+
+    while not team_menu_user_selected:
+        try:
+            team_menu_selected_option = int(input(f"\nEnter an option > "))
+            if team_menu_selected_option < 1 or team_menu_selected_option > number_of_teams:
+                print(f"Please enter a number between 1 and {number_of_teams}")
+            else:
+                print(f"Team: {teams[team_menu_selected_option - 1]} Stats")
+                print(f"--------------------")
+                print(f"Total players: ")
+                print(f"Total experienced: ")
+                print(f"Total inexperienced: ")
+                print(f"Average height: ")
+                team_menu_user_selected = True
+        except ValueError:
+            print('Numbers only')
+
+
     return players_only_set
 
+def display_menu():
+    print(f"\nBASKETBALL TEAM STATS TOOL\n")
+    print(f"------ MENU ------\n")
+    print(f"  Here are your choices:")
+    print(f"    1) Display Team Stats")
+    print(f"    2) Quit")
+    main_menu_user_selected = False
+
+    while not main_menu_user_selected:
+        try:
+            main_menu_selected_option = int(input("  Enter an option: "))
+            if main_menu_selected_option < 1 or main_menu_selected_option > 2:
+                print(f"Invalid option")
+            else:
+                main_menu_user_selected = True
+        except ValueError:
+            print('Numbers only')
+    return main_menu_selected_option
 
 # 2. Proper use of Dunder Main
 if __name__ == '__main__':
-    print()
-    # print(clean_data(PLAYERS))
-    balance_teams(PLAYERS, TEAMS)
-    #    display_menu()
-    # teamz = [
-    #     'Panthers',
-    #     ['player1', 'player2'],
-    #     'Bandits',
-    #     'Warriors',
-    # ]
-    # print(teamz[1][0])
-    #
-    # teamzz = {
-    #     'Panthers': '["Karl Saygan","Karl Saygan1"]'
-    # }
-    # print(teamzz['Panthers'])
-    # set1 = set()
-    # set2 = set(['foo', 'bar', 'baz', 'foo', 'qux'])
-    # set3 = set({'foo': 'bar'})
-    # set4 = set({})
-    # set5 = set2
-    #
-    # print(type(set1))
-    # print(type(set2))
-    # print(type(set3))
-    # print(type(set4))
-    # print(set5)
-    set1 = set(PLAYERS[0].items())
-    #print(set1)
+    clean_data(PLAYERS)
+    # balance_teams(PLAYERS, TEAMS)
+    main_menu_selected_option = display_menu()
+    if main_menu_selected_option == 1:
+        balance_teams(PLAYERS, TEAMS)
+    elif main_menu_selected_option == 2:
+        print(f"Exiting app... Have a nice day")
+
+
+
+"""
+DON"T USE FOR NOW
+
+def display_team_stats(players, teams):
+    number_of_teams = len(teams)
+    for idx, team_name_menu in enumerate(teams):
+        print(f" {idx + 1})  {team_name_menu}")
+
+    team_menu_user_selected = False
+
+    while not team_menu_user_selected:
+        try:
+            team_menu_selected_option = int(input(f"\nEnter an option > "))
+            if team_menu_selected_option < 1 or team_menu_selected_option > number_of_teams:
+                print(f"Please enter a number between 1 and {number_of_teams}")
+            else:
+                print(f"Team: {teams[team_menu_selected_option - 1]} Stats")
+                print(f"--------------------")
+
+                team_menu_user_selected = True
+        except ValueError:
+            print('Numbers only')
+"""
