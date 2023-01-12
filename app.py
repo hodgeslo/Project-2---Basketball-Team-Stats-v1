dcc41c798fe231803d331af79989d61ded0bdb54
hodgeslo <895933+hodgeslo@users.noreply.github.com>
2023-01-11 23:03:34 -0500

    Update app.py
    Submitting for project review prior to grading.

--- app.py
+++ app.py
@@ -1,34 +1,7 @@
-"""
-3. Import player and team data Import from constants.py the players and team data to be used within your program.
-Players and Teams should not be hard-coded in our script. If a team name or player name changes in constants.py,
-it should be reflected when we run our app.
-
-NOTE: Python has no concept of actual constants like other languages have. But it is a convention you may see in the
-real world. When you see ALL CAPS variable name you are meant to treat it as if it were a constant or a value that
-you cannot change/alter.
-"""
-
 from constants import TEAMS
 from constants import PLAYERS
 
 
-"""
-TEAMS = [
-    'Panthers',
-    'Bandits',
-    'Warriors',
-]
-
-PLAYERS = [{
-        'name': 'Karl Saygan',
-        'guardians': 'Heather Bledsoe',
-        'experience': 'YES',
-        'height': '42 inches'
-    },
-"""
-
-
-# 4. Create a clean_data function
 def clean_data(players):
     cleaned_players_list = []
     experienced_players = []
@@ -55,7 +28,6 @@ def clean_data(players):
     return cleaned_players_list
 
 
-# 5. Create a balance_teams function
 def balance_teams(players, teams):
     num_players_per_team = int(len(players) / len(teams))
     cleaned_players_list = clean_data(players[:])
@@ -67,6 +39,7 @@ def balance_teams(players, teams):
         complete_team_list.append([{balanced_team_list[m]:player_exp_list[:num_players_per_team].copy()}])
         del player_exp_list[:num_players_per_team]
 
+    # TODO:  Remove before project submission
     # print('\n\n\n')
     # print(f"*** THIS IS TEST LIST index 0:\n {complete_team_list[0][0].values()}\n")
     # print("Count of elements in the Nested Dictionary = ", sum(len(v) for v in complete_team_list[0][0].values()))
@@ -81,55 +54,44 @@ def balance_teams(players, teams):
     for idx, team_name_menu in enumerate(teams):
         print(f" {idx + 1})  {team_name_menu}")
 
-    # team_menu_user_selected = True
+    team_menu_user_selected = True
 
-    while True:
+    while team_menu_user_selected:
         try:
             team_menu_option = int(input(f"\nEnter an option > "))
             if team_menu_option < 1 or team_menu_option > number_of_teams:
                 print(f"Please enter a number between 1 and {number_of_teams}")
-            elif team_menu_option:
+            else:
                 team_menu_option = team_menu_option - 1
-                print(f" Team: {teams[team_menu_option]} Stats")
+                print(f"\n Team: {teams[team_menu_option]} Stats")
                 print(f" --------------------")
                 print(f" Total players: ", sum(len(v) for v in complete_team_list[team_menu_option][0].values()))
                 print(f" Total experienced: ", len([player_exp['experience'] for player_exp in complete_team_list[team_menu_option][0][teams[team_menu_option]] if player_exp['experience']]))
-                print(f" Total inexperienced: ", len([player_exp['experience'] for player_exp in complete_team_list[team_menu_option][0][teams[team_menu_option]] if not player_exp['experience']]))
-                print(f" Average height: ", sum([player_height['height'] for player_height in complete_team_list[team_menu_option][0][teams[team_menu_option]]]) / sum(len(v) for v in complete_team_list[team_menu_option][0].values()))
+                print(f" Total inexperienced: ", len([player_inexp['experience'] for player_inexp in complete_team_list[team_menu_option][0][teams[team_menu_option]] if not player_inexp['experience']]))
+                print(f" Average height: ", round(sum([player_height['height'] for player_height in complete_team_list[team_menu_option][0][teams[team_menu_option]]]) / sum(len(v) for v in complete_team_list[team_menu_option][0].values()), 2), "inches")
                 print(f" Players on Team:")
                 print(*[player_name['name'] for player_name in complete_team_list[team_menu_option][0][teams[team_menu_option]]], sep=", ")
-
-                # Display the guardians with a helper function
                 print(f" Guardians:")
-                the_guardians = [(guardian_name['guardians']) for guardian_name in complete_team_list[team_menu_option][0][teams[team_menu_option]]]
+                player_guardians = [(guardian_name['guardians']) for guardian_name in complete_team_list[team_menu_option][0][teams[team_menu_option]]]
+                # Display the_guardians with a helper function
+                print(*list(flatten_list(player_guardians)), sep=", ")
 
-                # Flatten lists with unequal levels of nesting
-                # Source and credit to https://www.youtube.com/watch?v=p9Cp5w2HV7g
-                def flatten_list(x):
-                    for item in x:
-                        if type(item) in [list]:
-                            for num in flatten_list(item):
-                                yield (num)
-                        else:
-                            yield (item)
-                return print(*list(flatten_list(the_guardians)), sep=", "), input("Press ENTER to Continue")
-
-                print(display_menu())
-                # end credit source
-            # else:
-            #     continue
-            #     # input("Press ENTER to Continue")
-            #
-            #     input("Press ENTER to Continue")
-            #     user_continued = input("Press ENTER to Continue")
-            #     if user_continued == "":
-            #         print(display_menu())
-            #     else:
-            #         input("Press ENTER to Continue")
+                team_menu_user_selected = False
+                input("\n\nPress ENTER to Continue")
         except ValueError:
             print('Numbers only')
 
-
+# Flatten lists with unequal levels of nesting
+# Source and credit to https://www.youtube.com/watch?v=p9Cp5w2HV7g
+def flatten_list(x):
+    for item in x:
+        if type(item) in [list]:
+            for num in flatten_list(item):
+                yield (num)
+        else:
+            yield (item)
+    return x
+# end credit source
 
 
 def display_menu():
@@ -151,13 +113,13 @@ def display_menu():
             print('Numbers only')
     return main_menu_selected_option
 
-# 2. Proper use of Dunder Main
-
 
 if __name__ == '__main__':
-    main_menu_selected_option = display_menu()
-    if main_menu_selected_option == 1:
-        clean_data(PLAYERS)
-        balance_teams(PLAYERS, TEAMS)
-    elif main_menu_selected_option == 2:
-        print(f"Exiting app... Have a nice day")
+    while True:
+        main_menu_selected_option = display_menu()
+        if main_menu_selected_option == 1:
+            clean_data(PLAYERS)
+            balance_teams(PLAYERS, TEAMS)
+        elif main_menu_selected_option == 2:
+            print(f"Exiting app... Have a nice day")
+            break
